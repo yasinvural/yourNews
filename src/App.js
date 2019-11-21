9c5e7b63098f54b63663260c502ca0bc778aca18
yasinvural <yasinvural0@gmail.com>
2019-11-21 21:14:17 +0300

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <>
       <h2 style={{textAlign:'center'}}>Hello News !</h2>
-      <div className="deneme">
-        <div className="deneme2"></div>
-      </div>
     </>
   );
 }
