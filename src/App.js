f2e14c9bddeda1e744669bc3d91a9736f2c7d37c
yasinvural <yasinvural0@gmail.com>
2019-11-22 18:43:12 +0300

    react router is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <>
-      <h2 style={{textAlign:'center'}}>Hello News !</h2>
+      <h2 style={{ textAlign: "center" }}>Hello News !</h2>
     </>
   );
 }
