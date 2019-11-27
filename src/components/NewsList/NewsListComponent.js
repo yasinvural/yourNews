0357dd897d006a1f53993d51e01c43ec556dfeb1
yasinvural <yasinvural0@gmail.com>
2019-11-27 17:39:04 +0300

    infinite scroll func. is added

--- src/components/NewsList/NewsListComponent.js
+++ src/components/NewsList/NewsListComponent.js
@@ -15,11 +15,15 @@ const styles = {
 
 const NewsListComponent = () => {
   const size = 10;
+  const totalCount = 14;
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(0);
+  const [requestSent, setRequestSent] = useState(false);
 
   useEffect(() => {
+    if (requestSent) return;
+
     const result = getNews({
       pagination: {
         page,
@@ -27,35 +31,45 @@ const NewsListComponent = () => {
       }
     });
     setLoading(true);
+    setRequestSent(true);
     result
       .then(data => {
-        setNews(n=> [...n, ...data]);
+        setNews(n => [...n, ...data]);
         setTimeout(() => {
           setLoading(false);
+          setRequestSent(false);
         }, 0);
       })
       .catch(err => {
         console.warn(err);
         setLoading(false);
+        setRequestSent(false);
       });
   }, [page]);
 
   useEffect(() => {
-    window.addEventListener('scroll',handleOnScroll);
+    window.addEventListener("scroll", handleOnScroll);
     return () => {
-      window.removeEventListener('scroll',handleOnScroll);
+      window.removeEventListener("scroll", handleOnScroll);
     };
-  })
+  });
 
   const handleOnScroll = () => {
-    const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-    const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
-    const clientHeight = document.documentElement.clientHeight || window.innerHeight;
-    const scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
-    if(scrolledToBottom){
-      console.log('bottom');
-      setPage(p=> p+1);
-      return;
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    const scrollHeight =
+      (document.documentElement && document.documentElement.scrollHeight) ||
+      document.body.scrollHeight;
+    const clientHeight =
+      document.documentElement.clientHeight || window.innerHeight;
+    const scrolledToBottom =
+      Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+    if (scrolledToBottom) {
+      if (totalCount != news.length) {
+        setPage(p => p + 1);
+        return;
+      }
     }
   };
 
