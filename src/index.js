4efbb3badefadac8710ecb1388c45c8436ee8df1
yasinvural <yasinvural0@gmail.com>
2019-12-13 21:37:39 +0300

    scss files are created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./styles/override.css";
+import "./styles/index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
