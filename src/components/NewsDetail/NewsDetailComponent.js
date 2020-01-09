f9d66d1aa93fa001d7c3371e06438643fe8db696
yasin vural <yasinvural0@gmail.com>
2020-01-08 20:39:48 -0800

    comment section is added to the news detail page

--- src/components/NewsDetail/NewsDetailComponent.js
+++ src/components/NewsDetail/NewsDetailComponent.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getNewsById } from "../../services/NewsService";
+import { getNewsComment } from "../../services/CommentService";
+import moment from "moment";
+import { Paper } from "@material-ui/core";
 
 const NewsDetailComponent = () => {
   const [newsDetail, setNewsDetail] = useState({});
+  const [newsComment, setNewsComment] = useState([]);
+  const [totalCommentCount, setTotalCommentCount] = useState(0);
+
   const params = useParams();
 
   useEffect(() => {
@@ -11,12 +17,53 @@ const NewsDetailComponent = () => {
       const result = await getNewsById(params.id);
       setNewsDetail(result.data);
     }
+    async function fetchComments() {
+      const result = await getNewsComment({ newsId: params.id });
+      setNewsComment(result.data);
+      setTotalCommentCount(result.totalCount);
+    }
     fetchNews();
+    fetchComments();
   }, [params.id]);
+
+  console.log(newsDetail);
+  console.log(newsComment);
+
+  const renderCommentSection = () => {
+    return (
+      <div className="comment-section">
+        <div className="comment-section__header">
+          Comments({totalCommentCount})
+        </div>
+        <div className="comment-section__comments">
+          {newsComment.map(comment => (
+            <div className="comment-container">
+              <div className="comment-container__owner">
+                <img src={comment.ownerProfilePhotoUrl} alt="" />
+              </div>
+              <div className="comment-container__body">
+                <div className="comment-container__body__username">
+                  {comment.ownerUsername}
+                </div>
+                <div className="comment-container__body__comment">
+                  {comment.body}
+                </div>
+                <div className="comment-container__body__date">
+                  {moment(comment.createdAt, "YYYY-MM-DD").fromNow()}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="news-detail">
-      <pre>{JSON.stringify(newsDetail, null, 2)}</pre>
+      <Paper elevation={2}>
+        <section>{renderCommentSection()}</section>
+      </Paper>
     </div>
   );
 };
--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -1,4 +1,5 @@
 import { baseService } from "./BaseService";
+import { createQueryFromObject } from "../utils/queryBuilder";
 
 export const deleteNewsComment = id => {
   return baseService.delete(`news-comments/${id}`);
@@ -6,4 +7,10 @@ export const deleteNewsComment = id => {
 
 export const createNewsComment = data => {
   return baseService.post("comments", data);
+};
+
+export const getNewsComment = (options = {}) => {
+  const query = createQueryFromObject(options);
+  const result = baseService.get(`comments?${query}`);
+  return result;
 };
